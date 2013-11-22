2401857508919d6c4cc1b43baa2dfee6704d90e0
Benedek_Kiss <Benedek_Kiss@epam.com>
2013-11-22 18:45:50 +0100

    TargetingSystem is empty - further thoughts are needed

--- src/main/java/com/epam/livingpope/torpedo/targeting/TargetingSystem.java
+++ src/main/java/com/epam/livingpope/torpedo/targeting/TargetingSystem.java
@@ -1,10 +1,6 @@
 package com.epam.livingpope.torpedo.targeting;
 
-import com.epam.livingpope.torpedo.shapes.Point;
-import com.epam.livingpope.torpedo.shapes.Ship;
-
 public interface TargetingSystem {
-    public Point generatePoint();
 
-    public Ship generateShip();
+    // TODO
 }
