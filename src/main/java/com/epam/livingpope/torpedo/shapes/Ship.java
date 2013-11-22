9bce1e64d3194799f5dcb243ca180ca333b7aa9d
Benedek_Kiss <Benedek_Kiss@epam.com>
2013-11-22 17:30:42 +0100

    Ship one thought

--- src/main/java/com/epam/livingpope/torpedo/shapes/Ship.java
+++ src/main/java/com/epam/livingpope/torpedo/shapes/Ship.java
@@ -3,49 +3,81 @@ package com.epam.livingpope.torpedo.shapes;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Ship {
+public class Ship implements Hittable {
 
-    private static final String SHIP_DOES_NOT_CONTAIN_POINT = "Ship does not contain point: ";
-    private final ArrayList<Point> pointList;
+    private final ArrayList<Field> shipFieldList = new ArrayList<>();
+    private final ShipShape shape;
 
-    public Ship(ArrayList<Point> pointList) {
-        this.pointList = pointList;
+    public Ship(ShipShape shape, int offsetX, int offsetY) {
+        this.shape = shape;
+        createShipFieldList(shape, offsetX, offsetY);
     }
 
-    // TODO
-    // could be removed with some effort
-    public ArrayList<Point> getPointList() {
-        return pointList;
-    }
-
-    public boolean hasPoint(Point point) {
-        return pointList.contains(point);
-    }
-
-    public void hitPoint(Point target) {
-        if (!hasPoint(target)) {
-            throw new IllegalArgumentException(SHIP_DOES_NOT_CONTAIN_POINT + target);
+    private void createShipFieldList(ShipShape shape, int offsetX, int offsetY) {
+        Field[][] fields = shape.fields;
+        for(int rowIndex=0;rowIndex<fields[0].length;rowIndex++) {
+            for(int colIndex=0;colIndex<fields.length;colIndex++) {
+                if(FieldState.UNHIT_SHIP.equals(fields[rowIndex][colIndex])) {
+                    shipFieldList.add(new Field(new Point(rowIndex + offsetY, colIndex + offsetX), FieldState.UNHIT_SHIP));
+                }
+            }
         }
-        handleHit(target);
     }
 
-    private void handleHit(Point point) {
-        pointList.remove(point);
+    @Override
+    public boolean isHittable(Point point) {
+        boolean result = false;
+        for (Field field : shipFieldList) {
+            if(field.isHittable(point)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void hit(Point point) {
+        for (Field field : shipFieldList) {
+            if(field.isHittable(point)) {
+                field.hit(point);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public boolean isUnHit() {
+        boolean result = false;
+        for (Field field : shipFieldList) {
+            if(!field.isUnHit()) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 
     public boolean isSunk() {
-        return pointList.isEmpty();
+        boolean result = false;
+        for (Field field : shipFieldList) {
+            if(field.isUnHit()) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 
     public boolean hasConflictWithShip(Ship ship) {
-        return !Collections.disjoint(pointList, ship.pointList);
+        return !Collections.disjoint(shipFieldList, ship.shipFieldList);
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((pointList == null) ? 0 : pointList.hashCode());
+        result = prime * result + ((shipFieldList == null) ? 0 : shipFieldList.hashCode());
         return result;
     }
 
@@ -58,17 +90,17 @@ public class Ship {
         if (getClass() != obj.getClass())
             return false;
         Ship other = (Ship) obj;
-        if (pointList == null) {
-            if (other.pointList != null)
+        if (shipFieldList == null) {
+            if (other.shipFieldList != null)
                 return false;
-        } else if (!pointList.equals(other.pointList))
+        } else if (!shipFieldList.equals(other.shipFieldList))
             return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Ship [pointList=" + pointList + "]";
+        return "Ship [pointList=" + shipFieldList + "]";
     }
 
 }
