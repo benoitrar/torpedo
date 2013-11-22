ef1f9ea6244a6239ed9b4cd4c233efc5309a5ae3
Benedek_Kiss <Benedek_Kiss@epam.com>
2013-11-22 17:34:02 +0100

    ShipShape one thought

--- src/main/java/com/epam/livingpope/torpedo/shapes/ShipShape.java
+++ src/main/java/com/epam/livingpope/torpedo/shapes/ShipShape.java
@@ -1,20 +1,41 @@
 package com.epam.livingpope.torpedo.shapes;
-import java.util.Arrays;
 
 public class ShipShape {
 
-    public static boolean[][] getShipForLength(int length) {
-        boolean array[] = new boolean[length];
-        Arrays.fill(array, true);
-        return new boolean[][] { array };
+    public final Field[][] fields;
+
+    public ShipShape(Field[][] fields) {
+        this.fields = fields;
+    }
+    
+    public ShipShape(boolean[][] values) {
+        validateValues(values);
+        fields = createFieldsWithValidValues(values);
     }
 
-    public static boolean[][] getSpecialShip() {
-        return new boolean[][] { { false, true, false }, { true, true, true } };
+    private Field[][] createFieldsWithValidValues(boolean[][] values) {
+        Field fields[][] = new Field[values[0].length][values.length];
+        for(int i=0;i<values[0].length;i++) {
+            for(int j=0;j<values.length;j++) {
+                fields[i][j] = createInitialFieldFromValue(values, i, j);
+            }
+        }
+        return fields;
     }
 
-    public static boolean[][] getShipFromFile() {
+    private Field createInitialFieldFromValue(boolean[][] values, int i, int j) {
+        Field field;
+        if(values[i][j]) {
+            field = new Field(FieldState.UNHIT_SHIP);
+        } else {
+            field = new Field(FieldState.EMPTY);
+        }
+        return field;
+    }
 
-        return new boolean[][] { {} };
+    private void validateValues(boolean[][] fields) {
+        if(fields.length == 0 || fields[0].length == 0) {
+            throw new IllegalArgumentException("Zero length field are prohibited");
+        }
     }
 }
