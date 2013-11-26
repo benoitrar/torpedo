86b6493f5178f6b8a13de43b92a5affccc336591
Benedek_Kiss <Benedek_Kiss@epam.com>
2013-11-26 14:53:05 +0100

    javadoc to Hittable interface addded

--- src/main/java/com/epam/livingpope/torpedo/shapes/Hittable.java
+++ src/main/java/com/epam/livingpope/torpedo/shapes/Hittable.java
@@ -2,6 +2,11 @@ package com.epam.livingpope.torpedo.shapes;
 
 public interface Hittable {
 
+    /**
+     * Tells if given point is an unhit ship.
+     * 
+     * @return true if point is part of an unhit ship
+     */
     boolean isHittable(Point point);
     
     void hit(Point point);
