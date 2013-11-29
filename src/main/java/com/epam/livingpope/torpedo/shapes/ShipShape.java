d2eb28a578d99ae03a412363563e8063f23a04fa
Livia Erdelyi <livia_erdelyi@epam.com>
2013-11-29 17:35:38 +0100

    under refactor

--- src/main/java/com/epam/livingpope/torpedo/shapes/ShipShape.java
+++ src/main/java/com/epam/livingpope/torpedo/shapes/ShipShape.java
@@ -5,6 +5,59 @@ public class ShipShape {
     public final FieldState[][] fields;
 
     public ShipShape(FieldState[][] fields) {
-        this.fields = fields;
+        FieldState[][] cutFields = cutFields(fields);
+        this.fields = cutFields;
+    }
+
+    private FieldState[][] cutFields(FieldState[][] unCutFields) {
+        FieldState[][] cutFields = cutHorizontally(fields);
+        cutFields = cutVertically(fields);
+        return cutFields;
+    }
+
+    private FieldState[][] cutHorizontally(FieldState[][] unCutFields) {
+        for (int i = 0; i < unCutFields.length; i++) {
+            if (isEmptyRow(unCutFields, i)) {
+                unCutFields = deleteEmptyRow(unCutFields, i);
+            }
+        }
+        return unCutFields;
+    }
+
+    private FieldState[][] deleteEmptyRow(FieldState[][] unCutFields, int rowNum) {
+        FieldState[][] result = new FieldState[getWidth(unCutFields) - 1][getHeight(unCutFields)];
+        // TODO
+    }
+
+    private boolean isEmptyRow(FieldState[][] unCutFields, int rowNum) {
+        boolean result = true;
+        for (int colNum = 0; colNum < unCutFields[rowNum].length; colNum++) {
+            if (unCutFields[rowNum][colNum].equals(FieldState.UNHIT_SHIP)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private FieldState[][] cutVertically(FieldState[][] unCutFields) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int getWidth(FieldState[][] fields) {
+        return fields[0].length;
+    }
+
+    public int getHeight(FieldState[][] fields) {
+        return fields.length;
+    }
+
+    public int getWidth() {
+        return getWidth(fields);
+    }
+
+    public int getHeight() {
+        return getHeight(fields);
     }
 }
